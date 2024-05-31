c5902caaa262f370a22725ea4c0e5ba03df2a2bd
Armando Di Santo <armando.di.santo@hotmail.com>
2024-05-31 22:44:18 +0200

    update
    funzione semplificata dopo aver visto il video dimostrativo

--- main.js
+++ main.js
@@ -1,39 +1,18 @@
 function contaCifre() {
-    let numero = prompt("Inserisci un numero intero positivo (massimo 9999):");
-
-    // Converti l'input in un numero intero
-    numero = parseInt(numero);
-
-
-
-    // Verifica se il numero è maggiore di 9999
-    while (numero > 9999) {
-        numero = prompt("Numero troppo grande. Inserisci un numero intero positivo (massimo 9999):");
-
-    }
-
-    // Verifica se è un numero intero positivo
-    while (numero <= 0) {
-        numero = prompt("Inserisci un numero intero positivo valido (massimo 9999):");
-    }
-
-
-    // Calcola il numero di cifre contando la lunghezza della stringa
-    let lunghezza = numero.toString().length;
-
-    //risultato
-    switch (lunghezza) {
-        case 1:
-            return '1 cifra';
-        case 2:
-            return '2 cifre';
-        case 3:
-            return '3 cifre';
-        case 4:
-            return '4 cifre';
-
+    let num = prompt("Inserisci un numero intero positivo:");
+    if (num < 10) {
+        return "1 cifra"
+    } else if (num < 100) {
+        return "2 cifre"
+    } else if (num < 1000) {
+        return "3 cifre"
+    } else if (num < 10000) {
+        return "4 cifre"
+    } else {
+        return "numero troppo grande"
     }
 }
+
 
 console.log(contaCifre());
 
